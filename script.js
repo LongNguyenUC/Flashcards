fdf163f652d68890affd645c172267711ba92063
Long Nguyen <192123324+LongNguyenUC@users.noreply.github.com>
2025-03-28 23:30:49 -0400

    Add display of flashcards for first card

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let cards = [];
 const term = document.querySelector("#term");
 const def = document.querySelector("#def");
 const addButton = document.querySelector("#addCard");
+const backButton = document.querySelector("#back");
+const nextButton = document.querySelector("#next");
+const cardDisplay = document.querySelector(".card-container");
+let currentCard = 0;
 
 function Flashcard(term, def){
     if(!new.target){
@@ -9,9 +13,26 @@ function Flashcard(term, def){
     }
     this.term = term;
     this.def = def;
-    
+    this.isFrontFaced = true;
 }
 
 addButton.addEventListener('click', () =>{
     cards.push(new Flashcard(term.value, def.value));
+    if (cards.length === 0){
+
+    }
 })
+
+cardDisplay.addEventListener('click', () => {
+    if(cards.length > 0){
+        if (cards[currentCard].isFrontFaced){
+            cards[currentCard].isFrontFaced = false;
+            cardDisplay.textContent = cards[currentCard].def;
+        }else{
+            cards[currentCard].isFrontFaced = true;
+            cardDisplay.textContent = cards[currentCard].term;
+        }
+    }
+})
+
+
